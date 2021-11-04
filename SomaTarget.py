b20e388a877f9aee0c0347f2ffc191f04d806982
Maaath <macedo.matheus81@gmail.com>
2021-11-04 18:16:30 -0300

    end

--- SomaTarget.py
+++ SomaTarget.py
@@ -8,15 +8,14 @@
 from genetic2020 import *
 from Mochila import mochila_main
 
-itens = [[2,12],[1,10],[3,20],[2,15],[4,25],[5,30],[1,11],[2,13],
-         [6,32],[7,20],[8,20],[9,15],[10,25],[15,20],[17,22],[20,29]] # item com o seu peso e valor [peso,valor]
-peso = 20 #kg
+itens = [[2,12],[1,10],[3,20],[2,15]] # item com o seu peso e valor [peso,valor]
+peso = 5 #kg
 target = mochila_main(itens,peso) # número alvo (melhor solução para esse conjunto de itens)
 i_length = len(itens) #posições do vetor
 i_min = 0 #range mínimo (item não contido)
 i_max = 1 #range máximo (item não contido)
-p_count = 40 #individuos por geração
-epochs = 1000 #épocas, quantas gerações eu vou gerar
+p_count = 20 #individuos por geração
+epochs = 20 #épocas, quantas gerações eu vou gerar
 p = population(p_count, i_length, i_min, i_max) #criando a minha população
 fitness_history = [media_fitness(p, itens, peso),] #histórico de confiabilidade de cada indivíduo
 
